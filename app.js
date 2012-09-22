54eb744319ecf9d431194c1dadaba2ff23f376fd
Akeda Bagus <akeda@x-team.com>
2012-09-22 22:01:34 +0700

    Handling pygmentize properly via child_process spawn.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require("express")
     , redis = require("redis")
     , rc = redis.createClient()
     , langs = require('./langs')
-    , exec = require('child_process').exec;
+    , cp = require('child_process');
 
 
 /**
@@ -17,6 +17,7 @@ app.configure(function() {
     app.set('view engine', 'jade');
     
     // Middleware
+    app.use(express.static(__dirname + '/assets'));
     app.use(express.bodyParser());
     app.use(app.router);
 });
@@ -33,26 +34,49 @@ app.get('/add', function(req, res) {
 app.post('/add', function(req, res) {
     rc.incr("nextId", function(err, id) {
         if (!err) {
-            // Spawn pygmentize and save the result via set
-            exec("echo '" + req.param('snip', '') + "' | pygmentize -l " + req.param('lang', 'html') +
-                 " -f html -O full,style=pastie -P title='Snippet #" + id + "'",
-                function(err, stdout, stderr) {
-                    if (!err) {
-                        rc.set('snips:id:' + id, JSON.stringify(stdout), function(err, reply) {
-                            if (!err) {
-                                // redirect to newly added snip
-                                res.redirect('/view/' + id);
-                            } else {
-                                // TODO: give 500 page, something wrong during set snip
-                                console.log(err);
-                            }
-                        });
-                    } else {
-                        // TODO: give 500 page, something wrong during pygmentize call
-                        console.log(err);    
-                    }
+            // Save raw snippet
+            rc.set('raw_snips:id:' + id, JSON.stringify(req.param('snip', '')), function(err, reply) {
+                if (err) {
+                    console.log(err);
                 }
-            );
+            });
+
+            var out = "";
+            // Spawn pygmentize and save the result via set
+            var pyg = cp.spawn("pygmentize", ["-l", req.param('lang', 'html')
+                ,"-f", "html"
+                ,"-O", "'style=pastie'"
+            ]);
+            pyg.stdout.on("data", function(data) {
+                console.log("pygmentize: on data");
+                out += data;
+            });
+            pyg.on("exit", function(code) {
+                console.log("pygmentize: exiting")
+                if (code !== 0) {
+                    console.log("pygmentize process exited with code " + code);
+                } else {
+                    // Save highlighted snippet
+                    rc.set('snips:id:' + id, JSON.stringify(out), function(err, reply) {
+                        if (!err) {
+                            // redirect to newly added snip
+                            res.redirect('/view/' + id);
+                        } else {
+                            // TODO: give 500 page, something wrong during set snip
+                            console.log(err);
+                        }
+                    });
+                }
+                pyg.stdin.end();
+            });
+
+            pyg.stderr.on("data", function(data) {
+                console.log("pygmentize err: " + data);
+                pyg.stdin.end();
+            });
+
+            pyg.stdin.write(req.param('snip', ''));
+            pyg.stdin.end();
         } else {
             // TODO: give 500 page, unable to inc nextId
             console.log(err);
@@ -60,15 +84,35 @@ app.post('/add', function(req, res) {
     });
 });
 
-app.get('/view/:id', function(req, res) {
-    rc.get("snips:id:" + req.params.id, function(err, snip) {
-        if (!err) {
-            res.send(JSON.parse(snip));
-        } else {
-            // TODO: give 500 page, unable to find snip with given id
-            console.log(err);
-        }
-    });
+app.get('/view/:id/:format?', function(req, res) {
+    var id = req.params.id
+        , format = req.params.format;
+
+    switch(format) {
+        case "raw":
+            rc.get("raw_snips:id:" + id, function(err, snip) {
+                if (!err) {
+                    res.header("Content-Type", "text/plain; charset=utf-8");
+                    res.send(JSON.parse(snip));
+                } else {
+                    // TODO: give 404 page, unable to find raw_snip
+                    console.log(err);
+                }
+            });
+            break;
+        default:
+            rc.get("snips:id:" + id, function(err, snip) {
+                if (!err) {
+                    res.render('view', {
+                        id: id
+                        , snip: JSON.parse(snip)
+                    });
+                } else {
+                    // TODO: give 404 page, unable to find snip with given id
+                    console.log(err);
+                }
+            });
+    }
 });
 app.get('/', function(req, res) {
     res.render('index', {
